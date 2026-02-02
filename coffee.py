a2054c480e74e649d94ca26fb0cb2601a9b0a0b8
dylan lewis <djl51@kent.ac.uk>
2026-02-02 14:43:52 +0000

    2

--- coffee.py
+++ coffee.py
@@ -1,12 +1,23 @@
-price = 75
-accepted_coin = [50,25,10,5]
+#functions, the coffee machine must accept coins (intergers) 50p and lower so 50,25,10,5,2,1p as coins are inseted
+#take the remainder of the price and make it the new total price un till the total is achived then it need to 
+#give back the change 
+
+#non-functions, the machine needs to be able to turn away non intergers and ask for the correct input which is an 
+#price
+
+
+
+
+
+price = 99
+accepted_coin = [50,25,10,5,2,1]
 
 print("Welcome to Unit_CoffeeMachine ")
 
 while price > 0: 
     print(f"price:{price}p")
 
-    coin = int(input("insert coins now (50,25,10,5): "))
+    coin = int(input("insert coins now (50,25,10,5,2,1): "))
 
     if coin in accepted_coin:
         price -= coin 
@@ -15,6 +26,7 @@ while price > 0:
 
 print("_" * 20)
 print("procesing payment")
+print("be paciant")
 print("purchase compleate")
 print("thank you for your purchase")
 print("please enjoy")
