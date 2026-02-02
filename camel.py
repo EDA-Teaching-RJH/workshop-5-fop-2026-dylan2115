6a7700eddebe9811c40f705595566406a7c02897
dylan lewis <djl51@kent.ac.uk>
2026-02-02 13:37:03 +0000

    1

--- camel.py
+++ camel.py
@@ -1,1 +1,11 @@
-# Enter your code here
+def main():
+    userInput = input("what is your preferd first name")
+
+    snakeCaseName = ""
+    for char in userInput:
+        if char.isupper():
+            snakeCaseName += "_" + char.lower()
+        else:
+            snakeCaseName += char
+    print(snakeCaseName)
+main()
